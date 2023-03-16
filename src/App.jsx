9005b2693343083c5db0f7747f9ee64b5987aca1
Caitlin Dawson <daws0104@algonquinlive.com>
2023-03-16 18:26:28 -0400

    updates values

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import FooterInfo from './components/FooterInfo'
 import HeaderInfo from './components/HeaderInfo'
 import Tasks from './components/Tasks'
 import Greeting from './components/Greeting'
+import Form from './components/Form'
+import NewTaskForm from './components/NewTaskForm'
+import listdata from '../listdata';
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const [allListItems, setAllListItems] = React.useState(listdata)
 
   return (
     <div className="App">
       <Greeting />
       <HeaderInfo />
-      {/* <App /> */}
-      <Tasks />
+      <Tasks allListItems={allListItems} />
+      <Form allListItems={allListItems} setAllListItems={setAllListItems} />
+      <NewTaskForm />
       <FooterInfo />
     </div>
   );
